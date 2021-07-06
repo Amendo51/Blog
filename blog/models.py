d3a0c491dcae5f66a85e9ff6bf149e5a6a08027e
alejandro <amendo51@uic.edu>
2021-07-06 13:47:18 -0500

    initial commit

---
+++ blog/models.py
@@ -0,0 +1,16 @@
+from django.db import models
+from django.urls import reverse
+
+# Create your models here.
+class Post(models.Model):
+    title = models.CharField(max_length=200) #charfield is for smaller titles like name ex: title of the blog
+    author = models.ForeignKey( #foreign key is a key share in two table that might be named differantly
+        'auth.User',
+        on_delete=models.CASCADE,
+    )
+    body = models.TextField()
+    def __str__(self):
+        return self.title
+
+    def get_absolute_url(self):  # new
+        return reverse('post_detail', args=[str(self.id)])
